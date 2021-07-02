950403a9ad811c00cb2993956f86cb0cd32b1732
Srinjana Pathak <srinjanap.official@gmail.com>
2021-07-02 21:35:42 +0530

    local files updated

--- video_steganography.py
+++ video_steganography.py
@@ -27,24 +27,21 @@ def split_string(s_str, count=10):
     return split_list
 
 
-def frame_extraction(video):
-    if not os.path.exists("./tmp"):
-        os.makedirs("tmp")
-
-    temp_folder = "./tmp"
-    print("[INFO] tmp directory has been successfully created.")
-
-    vidcap = cv2.VideoCapture(video)
-    count = 0
-
-    while True:
-        success, image = vidcap.read()
-        if not success:
-            break
-        cv2.imwrite(os.path.join(temp_folder, "{:d}.png".format(count)), image)
-        count += 1
-
-
+def split_string(s_str, count=10):
+    per_c = math.ceil(len(s_str)/count)
+    c_cout = 0
+    out_str = ''
+    split_list = []
+    for s in s_str:
+        out_str += s
+        c_cout += 1
+        if c_cout == per_c:
+            split_list.append(out_str)
+            out_str = ''
+            c_cout = 0
+    if c_cout != 0:
+        split_list.append(out_str)
+    return split_list
 def encode_string(input_string, root="./tmp/"):
     split_string_list = split_string(input_string)
     for i in range(0, len(split_string_list)):
